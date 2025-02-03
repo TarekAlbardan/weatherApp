621959f95e0af71198dd4a52a995d82b4566641a
T9code <6rooo8sy@gmail.com>
2025-02-03 23:39:53 +0100

    Initial Commit

---
+++ weatherApp/weatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherAppApp.swift
+//  weatherApp
+//
+//  Created by MacBook on 2025-02-03.
+//
+
+import SwiftUI
+
+@main
+struct weatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
